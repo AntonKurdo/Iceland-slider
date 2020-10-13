8b7fe97705dc463383d9905da3d961dbbcfa24ef
Anton Kurdo <anton.kurdo@gmail.com>
2020-10-13 20:33:14 +0300

    Updates

---
+++ precache-manifest.f0f25732ffc9f16d15781cbf2f698ef1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496835637bca89c4a4cc8c2f998303c2",
+    "url": "/Iceland-slider/index.html"
+  },
+  {
+    "revision": "d6c37929d146aac4fb2a",
+    "url": "/Iceland-slider/static/css/main.d8d98724.chunk.css"
+  },
+  {
+    "revision": "26d542a4df5663febaea",
+    "url": "/Iceland-slider/static/js/2.2e1fa8a3.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/Iceland-slider/static/js/2.2e1fa8a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6c37929d146aac4fb2a",
+    "url": "/Iceland-slider/static/js/main.d45b9f96.chunk.js"
+  },
+  {
+    "revision": "b964b1c3c568d252ec26",
+    "url": "/Iceland-slider/static/js/runtime-main.98fcd579.js"
+  },
+  {
+    "revision": "334a207c3676d71ecc668a0ab6069ad3",
+    "url": "/Iceland-slider/static/media/1.334a207c.jpg"
+  },
+  {
+    "revision": "76798903baaa17e1e4096a4ad3a65554",
+    "url": "/Iceland-slider/static/media/2.76798903.jpg"
+  },
+  {
+    "revision": "f492a9dd975e34b0787000ce23ca6344",
+    "url": "/Iceland-slider/static/media/3.f492a9dd.jpg"
+  },
+  {
+    "revision": "e4c4d38e11631b5ef2bbdd0b7783b3bd",
+    "url": "/Iceland-slider/static/media/4.e4c4d38e.jpg"
+  },
+  {
+    "revision": "0ea2727d2d500d8212a8378220dfdf49",
+    "url": "/Iceland-slider/static/media/5.0ea2727d.jpg"
+  }
+]);
